468a380d6e2be1be1bc5357b45feee28a7b433b7
EliasDoosje <75253207+EliasDoosje@users.noreply.github.com>
2020-12-14 11:52:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Loser!")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(6)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(1, 6))
+})
